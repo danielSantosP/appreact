f1ccb0a9c3baede04e76f80ca7e0bb0de90c5a6b
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-09-29 17:06:22 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react'
-import Footer from './inicio/Components/Footer'
-import Form from './inicio/Components/Form/Form';
-import Header from './inicio/Components/Header'
 
-const Teste = () => {
-  const active = false
-  if(active){ 
-    return <p>Teste</p>
-  }else{
-    return null
-  }
-}
-
-const App = () => {
+function App() {
   return (
-    <>
-      <Teste />
-      <Header />
-      <Form />
-      <Footer />
-    </>
-  )
-};
+    <div>App React</div>
+  );
+}
 
 export default App;
