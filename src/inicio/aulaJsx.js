90dc2727134b2d37f8fbf9b62a2c72a13fe46e09
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-09-29 15:41:53 -0300

    basic jsx

---
+++ src/inicio/aulaJsx.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const titulo = <h1>Esse é um título</h1>
+
+const App = () => {
+  const random = Math.random()
+  const ativo = false
+
+function mostrarNome(sobrenome){
+  return 'Daniel ' + sobrenome
+}
+
+const carro = {
+  marca: 'Ford',
+  rodas: 4,
+}
+
+const estiloP = {
+  color: 'blue',
+  fontSize: "2rem"
+}
+
+  return (
+    <>
+      {titulo}
+      <p>{true?'sss':'nnn'} - {10}{mostrarNome('Santos')}</p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p className={ativo?'ativo':'inativo'}>{(random * 100) / 50}</p>
+    </>
+  )
+    
+}
+
+export default App;
