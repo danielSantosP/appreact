bae2bc09b1f2747463e5d2e4060a2accbce42c56
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-09-29 15:53:11 -0300

    JSX challenge

--- src/inicio/aulaJsx.js
+++ src/inicio/aulaJsx.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const titulo = <h1>Esse é um título</h1>
 
-const App = () => {
+const AulaJsx = () => {
   const random = Math.random()
   const ativo = false
 
@@ -33,4 +33,4 @@ const estiloP = {
     
 }
 
-export default App;
+export default AulaJsx;
