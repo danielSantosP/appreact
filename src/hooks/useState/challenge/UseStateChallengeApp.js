180fe7afcaa22de50445d4a861ac31b52a8b5dc6
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-09-30 10:27:36 -0300

    useState - challenge

---
+++ src/hooks/useState/challenge/UseStateChallengeApp.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Produto from './UseStateChallengeProduto'
+
+const UseStateChallengeApp = () => {
+
+  const [dados, setDados] = React.useState(null)
+  const [carregando, setCarregando] = React.useState(null)
+
+  async function  handleClick(event){
+    setCarregando(true)
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
+      )
+      
+      const json = await response.json()
+      
+      setDados(json)
+      setCarregando(false)
+  }
+
+  return (
+    <div>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Notebook</button>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Smartphone</button>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Tablet</button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
+    </div>
+  )
+}
+
+export default UseStateChallengeApp
